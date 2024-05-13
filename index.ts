aa4774223b6e013af73c48401e9522fc84ababb6
harisaltafb1o5 <haris_memon1@yahoo.com>
2024-05-13 22:21:44 +0500

    added a console

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import inquirer from "inquirer";
 //1) computer will generate a random number
 //2) user input for guessing number
 //3) compare user input wiyh comp gen. unmber and show result
-
+console.log ("Welcome to number guessing game")
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 
 const answers= await inquirer.prompt([
